89b0c5851f63f1a9cebef766eee250ec2296b029
NileshSP <emailnileshp@gmail.com>
2018-11-26 18:08:32 -0500

    minor revision

--- ReactAspnet/Models/SampleDatabaseContext.cs
+++ ReactAspnet/Models/SampleDatabaseContext.cs
@@ -8,24 +8,13 @@ using Microsoft.EntityFrameworkCore.Metadata;
 namespace ReactAspnet.Models
 {
     // DB context and relative models were created using nuget package manager console by executing the below command
-    // Scaffold-DbContext "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Nilesh\Source\Repos\ReactAspnet\Database\SampleDatabase.mdf;Integrated Security=True;Database=[SampleDB];" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -Tables Website, WebsiteVisitDetails -f
+    // Scaffold-DbContext "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={relativeRootPath}\App_Data\SampleDatabase.mdf;Integrated Security=True;Database=SampleDatabase;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -Tables Website, WebsiteVisitDetails -f
     public partial class SampleDatabaseContext : DbContext
     {
         public virtual DbSet<Website> Website { get; set; }
         public virtual DbSet<WebsiteVisitDetails> WebsiteVisitDetails { get; set; }
 
         public SampleDatabaseContext(DbContextOptions<SampleDatabaseContext> options) : base(options) { }
-
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    if (!optionsBuilder.IsConfigured)
-        //    {
-        //        var database = @"C:\Users\Nilesh\Source\Repos\ReactAspnet\Database\SampleDatabase.mdf"; // Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Database\SampleDatabase.mdf");
-        //        var connString = $"Data Source = (LocalDB)\\MSSQLLocalDB;AttachDbFilename={database};Database=[SampleDB];Integrated Security = True; Connect Timeout = 30";
-        //        // warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-        //        optionsBuilder.UseSqlServer(connString);
-        //    }
-        //}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
