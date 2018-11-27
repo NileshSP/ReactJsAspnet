8e4f761bb384ea747c50177fc1852a386d48316a
NileshSP <emailnileshp@gmail.com>
2018-11-26 19:11:36 -0500

    revised steps to publish website using mutiple options

--- herokudockerpublish.bat
+++ herokudockerpublish.bat
@@ -1,5 +1,20 @@
+::heroku.com publish commands
+
+::Using website publish
+::create heroku app using browser login
+::using heroku CLI in the root directory of the project execute following commands
+::heroku login
+::heroku git:remote -a reactjsaspnetcoresql
+::heroku buildpacks:set https://github.com/jincod/dotnetcore-buildpack
+::heroku buildpacks:add --index 1 heroku/nodejs
+::heroku buildpacks -- check for registered buildpacks for the repository/project
+::git subtree push --prefix ReactAspnet heroku master    OR    git push heroku master
+
+
+::Using container build locally & publish
+
 REM - This file assumes that you have access to the application and that you have docker installed
-REM : Setup your applications name below
+REM : Setup your applications name below using App_Name created on heroku.com
 SET APP_NAME="reactjsaspnetcoresql"
 
 REM - Delete all files and folders in publish
@@ -15,18 +30,9 @@ call heroku container:push web -a %APP_NAME%
 call heroku container:release web -a %APP_NAME%
 
 
-::heroku publish commands
-::create heroku app using browser using login
-::using heroku CLI in the root directory of the project
-::heroku login
-::heroku git:remote -a reactjsaspnetcoresql
-::heroku buildpacks:set https://github.com/jincod/dotnetcore-buildpack
-::heroku buildpacks:add --index 1 heroku/nodejs
-::heroku buildpacks -- check for registered buildpacks for the repository/project
-::git subtree push --prefix ReactAspnet heroku master    OR    git push heroku master
-
-::heroku build docker container at remote and publish app
+::Using container build at remote & publish
+::On heroku.com (free hosting services) build docker container at remote(in heroku platform) and publish app
 ::add heroku.yml file in the root with required commands
 ::commit git changes
 ::heroku stack:set container
-:: git push -f heroku master
+::git push -f heroku master
